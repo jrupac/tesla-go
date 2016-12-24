05a340317e96e63e90aab43afc1907c5467e160c
Ajay Roopakalu <ajay.roopakalu@gmail.com>
2016-12-24 10:40:16 -0500

    Add logging support with file rotation.

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"log"
+	"path"
 	"strconv"
 )
 
@@ -14,9 +16,14 @@ const (
 )
 
 var (
-	configPath      = flag.String("configPath", "config.json", "Path to configuration file.")
+	configPath      = flag.String("config_path", "config.json", "Path to configuration file.")
+	logDir          = flag.String("log_dir", "/var/log/tesla-go", "Directory of logging destination.")
+	logFilename     = flag.String("log_filename", "tesla-go.INFO", "Name of logging file.")
 	pushgatewayAddr = flag.String("pushgateway_address", "", "URL of pushgateway.")
-	odometerMetric  = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+)
+
+var (
+	odometerMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "odometric_metric",
 		Help: "Current odometer value per vehicle.",
 	}, []string{"vehicle_id", "vehicle_name"})
@@ -24,6 +31,14 @@ var (
 
 func main() {
 	flag.Parse()
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   path.Join(*logDir, *logFilename),
+		MaxSize:    10, // MiB
+		MaxBackups: 5,
+		LocalTime:  true,
+	})
+
 	log.Printf("Tesla Go version %v\n", version)
 
 	config := LoadConfig(*configPath)
@@ -50,5 +65,7 @@ func main() {
 
 	if err := push.Collectors(jobName, nil, *pushgatewayAddr, odometerMetric); err != nil {
 		log.Fatalf("Failed to push metrics: %s", err)
+	} else {
+		log.Println("Sucessfully pushed metrics.")
 	}
 }
