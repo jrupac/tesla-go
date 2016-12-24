4e50a4725c36fb8b6adbfcd3bfe85fdf1d5d0bd7
Ajay Roopakalu <ajay.roopakalu@gmail.com>
2016-12-23 23:50:43 -0500

    Add metric collection via Prometheus.

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"flag"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
 	"log"
+	"strconv"
 )
 
 const (
 	version = "0.01"
+	jobName = "tesla_collector"
 )
 
-var config_path = flag.String("config_path", "config.json", "Path to configuration file.")
+var (
+	configPath      = flag.String("configPath", "config.json", "Path to configuration file.")
+	pushgatewayAddr = flag.String("pushgateway_address", "", "URL of pushgateway.")
+	odometerMetric  = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "odometric_metric",
+		Help: "Current odometer value per vehicle.",
+	}, []string{"vehicle_id", "vehicle_name"})
+)
 
 func main() {
 	log.Printf("Tesla Go version %v\n", version)
 
-	config := LoadConfig(*config_path)
+	config := LoadConfig(*configPath)
 	client := NewTeslaClient()
 
 	if err := client.Authenticate(config); err != nil {
@@ -25,14 +36,18 @@ func main() {
 
 	vehicles, err := client.ListVehicles()
 	if err != nil {
-		log.Fatalf("Operation failed: %s", err)
+		log.Fatalf("ListVehicles operation failed: %s", err)
 	}
 
-	for _, vehicle := range vehicles {
-		resp, err := client.GetVehicleState(vehicle)
+	for _, v := range vehicles {
+		resp, err := client.GetVehicleState(v)
 		if err != nil {
-			log.Fatalf("Error while getting drive state of vehicle %s: %s", vehicle.ID, err)
+			log.Fatalf("Error while getting drive state of v %s: %s", v.ID, err)
 		}
-		log.Printf("%d: %+v\n", vehicle.ID, resp)
+		odometerMetric.WithLabelValues(strconv.FormatInt(v.ID, 10), v.DisplayName).Set(resp.Odometer)
+	}
+
+	if err := push.Collectors(jobName, nil, *pushgatewayAddr, odometerMetric); err != nil {
+		log.Fatalf("Failed to push metrics: %s", err)
 	}
 }
